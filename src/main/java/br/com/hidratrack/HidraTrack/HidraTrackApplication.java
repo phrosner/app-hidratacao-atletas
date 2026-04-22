29ca04a1b526f60c49ee87c7e153cbd2b0e0ef79
Pedro Henrique Rosner <phrosner09@gmail.com>
2026-04-22 10:40:28 -0300

    Projeto inicial Spring Boot HidraTrack

---
+++ src/main/java/br/com/hidratrack/HidraTrack/HidraTrackApplication.java
@@ -0,0 +1,13 @@
+package br.com.hidratrack.HidraTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HidraTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HidraTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hidratrack/HidraTrack/HidraTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hidratrack.HidraTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HidraTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
